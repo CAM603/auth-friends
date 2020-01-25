56c30cbffa5ff768436a94ae19ea62f0eb5ea69c
CAM603 <cameronhawley@comcast.net>
2020-01-25 18:12:17 -0500

    added axios object

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChanges = event => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const login = event => {
+        event.preventDefault()
+        // Custom axios call here
+    }
+
     return (
         <div>
             <h1>Login</h1>
+            <div>
+                <form>
+                    <input 
+                    type="text"
+                    placeholder="username"
+                    name="username"
+                    value={credentials.username}
+                    onChange={handleChanges}
+                    />
+                    <input 
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    value={credentials.password}
+                    onChange={handleChanges}
+                    />
+                    <button>Submit!</button>
+                </form>
+            </div>
         </div>
     )
 }
